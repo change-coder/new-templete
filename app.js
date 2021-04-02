481000208f9a2afeae9369c6e116c20241e11140
change-coder <80451758+change-coder@users.noreply.github.com>
2021-04-02 11:22:28 +0530

    count that matters

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-     (function() {
-        "use strict";
-      /**
-     * Toggle .header-scrolled class to #header when page is scrolled
-     */
-     let selectHeader = select('#header')
-     if (selectHeader) {
-       const headerScrolled = () => {
-         if (window.scrollY > 100) {
-           selectHeader.classList.add('header-scrolled')
-         } else {
-           selectHeader.classList.remove('header-scrolled')
-         }
-       }
-       window.addEventListener('load', headerScrolled)
-       onscroll(document, headerScrolled)
-     }
-       
-      
-      })();
+$(document).ready(function(){
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1200
+  })
+});    
